c2e01cca819571ea81d64584cd7f5b1e12456055
rjgodia <ahrjhey.godia@yahoo.com>
2016-01-14 13:50:31 -0800

    update1
    update1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
     <body>
     <?php
-
+        /* Sets the game board if unavailable */
         $game = new Game(!isset($_GET['board']) ? '---------' : $_GET['board']);
 
         if($game->winner('x'))
@@ -30,56 +30,41 @@
 <?php
 class Game
 {
-    var $position;
+    var $position; // an array that remembers the current game state of the board
 
     function __construct($squares)
     {
         $this->position = str_split($squares);
     }
 
-
-    function winner($token)
-    {
-        // horizontal
-        if(($this->position[0] == $token)&& ($this->position[1] == $token)&& ($this->position[2] == $token))
-            return true;
-        if(($this->position[3] == $token)&& ($this->position[4] == $token)&& ($this->position[5] == $token))
-            return true;
-        if(($this->position[6] == $token)&& ($this->position[7] == $token)&& ($this->position[8] == $token))
-            return true;
-
-        // vertical
-        if(($this->position[0] == $token)&& ($this->position[3] == $token)&& ($this->position[6] == $token))
-            return true;
-        if(($this->position[1] == $token)&& ($this->position[4] == $token)&& ($this->position[7] == $token))
-            return true;
-        if(($this->position[2] == $token)&& ($this->position[5] == $token)&& ($this->position[8] == $token))
-            return true;
-
-        // diagonal
-        if(($this->position[0] == $token)&& ($this->position[4] == $token)&& ($this->position[8] == $token))
-            return true;
-        if(($this->position[2] == $token)&& ($this->position[4] == $token)&& ($this->position[6] == $token))
-            return true;
-        return false;
-    }
-
-
-    /*
+    /* checks for the winner game state (whether it's horizontal, vertical or diagonal) */
     function winner($token)
     {
         $result = false;
 
+        /* horizontal */
         for($row=0; $row<3; $row++)
+        {
+            $result = true;
             for ($col = 0; $col < 3; $col++)
                 if ($this->position[3 * $row + $col] != $token)
                     $result = false;
 
-        for($row=0; $row<3; $row++)
-            for ($col = 0; $col < 3; $col++)
+            if($result) return true; // recognizes a whole row with the same token
+        }
+
+        /* vertical */
+        for($col=0; $col<3; $col++)
+        {
+            $result = true;
+            for ($row = 0; $row < 3; $row++)
                 if ($this->position[3 * $row + $col] != $token)
                     $result = false;
 
+            if($result) return true; // recognizes a column
+        }
+
+        /* diagonals */
         if(($this->position[0] == $token)&& ($this->position[4] == $token)&& ($this->position[8] == $token))
             $result = true;
         if(($this->position[2] == $token)&& ($this->position[4] == $token)&& ($this->position[6] == $token))
@@ -87,48 +72,49 @@ class Game
 
         return $result;
     }
-    */
 
-
-
+    /* a random move done by the cpu as an 'o' player */
     function pick_move()
     {
-        $cpu = array();
+        $cpu = array(); // temporary array for '-' cells
+
         for ($i = 0; $i < 9; $i ++)
             if($this->position[$i] == '-')
-                array_push($cpu, $i);
-        $this->position[$cpu[array_rand($cpu)]] = 'o';
+                array_push($cpu, $i); // remembers all the available cells
+
+        $this->position[$cpu[array_rand($cpu)]] = 'o'; // replaces a random available cell
     }
 
-
+    /* displays the current board state */
     function display()
     {
-        echo  '<table cols="3" style= "font­size:large; font­weight:bold">';
-        echo '<tr>';
+        echo  '<table cols="3" border = "1" style= "font­size:large; font­weight:bold" width="400" height = "300" >';
+        echo '<tr align = "center">';
 
         for($pos = 0; $pos < 9; $pos++)
         {
             echo $this->show_cell($pos);
-            if($pos % 3 == 2) echo '</tr><tr>';
+            if($pos % 3 == 2) echo '</tr><tr align = "center">'; // makes a new row for every 3 cells made
         }
 
         echo '</tr>';
         echo '</table>';
+
     }
 
+    /* shows the player's move in each cell */
     function show_cell($which)
     {
-
         $token = $this->position[$which];
 
         if($token <> '-')
-            return '<td>'.$token.'</td>';
+            return '<td width="33.33%" height = "33.33%"> '.$token.'</td>';
 
         $this->newposition = $this->position;
         $this->newposition[$which] = 'x';
         $move = implode($this->newposition);
         $link = 'index.php?board='.$move;
-        return '<td><a href="'.$link .'" style="text-decoration: none" >-</a></td>';
+        return '<td height = "33.33%" width="33.33%"><a href="'.$link .'" style="text-decoration: none" >-</a></td>';
 
     }
 }
